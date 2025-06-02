e7ca04d0fcbf5cb835ba5523bd4e6dcde4859079
olusegun <shegezzy@gmail.com>
2025-06-02 12:56:12 +0100

    Add Functions, Data Structures and Modules tasksand updated readme

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return "Error: Cannot divide by zero."
-            return num1 / num2
-        case _:
-            return "Error: Invalid operation."
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    else:
+        return "Invalid operation"
